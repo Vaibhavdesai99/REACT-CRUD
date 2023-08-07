29f9ba1ffc9022603025e18e3a773099b12ab8d3
Vaibhav Ramchandra Desai <127377578+Vaibhavdesai99@users.noreply.github.com>
2023-08-07 10:58:25 +0530

    Comment added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+// Imported React router dom
 import Create from "./Components/Create";
 import { Routes, Route } from "react-router-dom";
 import Read from "./Components/Read";
